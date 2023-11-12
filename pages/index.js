a37092b31911c119f11f7d127db99c621e29dbff
Ulises_Ortega <ulisses.ortega@hotmail.com>
2023-11-11 16:59:27 -0800

    Initial Commintment

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import Head from 'next/head';
+import Link from 'next/link'
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
+      <head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+            Read <Link href="/posts/first-post">this page!</Link>
         </h1>
 
         <p className={styles.description}>
